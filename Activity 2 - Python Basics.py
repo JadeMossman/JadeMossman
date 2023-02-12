d1bd9bbd252f16f14bc50b2651705e46d1760b2a
JadeMossman <123392459+JadeMossman@users.noreply.github.com>
2023-02-12 13:01:51 +0000

    Update Activity 2 - Python Basics.py

--- Activity 2 - Python Basics.py
+++ Activity 2 - Python Basics.py
@@ -1,5 +1,7 @@
 #Activity 2 - Python Basics
 
+#Write a Python program to divide two numbers, using the input from the user, and print the result on the screen.
+
 num1 = float(input("Enter a float value: "))
 #print(num1)
 num2 = float(input("Enter a float value: "))
