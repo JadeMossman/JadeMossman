e56d39c6193fcc2a40926b754ac9fec2fc8bab4e
JadeMossman <123392459+JadeMossman@users.noreply.github.com>
2023-02-12 11:59:20 +0000

    Add files via upload

---
+++ Activity 1 - Python Basics.py
@@ -0,0 +1,10 @@
+#Activity 1 - Python Basics
+
+(a)=-5 + 8 * 6
+print (a)
+(b)=(55+9) % 9
+print (b)
+(c)=20 + -3*5 / 8
+print (c)
+(d)=5 + 15 / 3 * 2 - 8 % 3
+print (d)
---
+++ Activity 2 - Python Basics.py
@@ -0,0 +1,6 @@
+#Activity 2 - Python Basics
+
+num1 = float(input("Enter a float value: "))
+print(num1)
+num2 = float(input("Enter a float value: "))
+print(num2)
