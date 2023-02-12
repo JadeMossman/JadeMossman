a17b9125b0cf95f936647f9ae71e1fc5f36cfc8c
JadeMossman <123392459+JadeMossman@users.noreply.github.com>
2023-02-12 12:30:16 +0000

    Update Activity 2 - Python Basics.py

--- Activity 2 - Python Basics.py
+++ Activity 2 - Python Basics.py
@@ -1,6 +1,7 @@
 #Activity 2 - Python Basics
 
 num1 = float(input("Enter a float value: "))
-print(num1)
+#print(num1)
 num2 = float(input("Enter a float value: "))
-print(num2)
+#print(num2)
+print (int(num1/num2))
